50833473858b362fa4096b2a9faed4b0ecc956df
koksing456 <koksing456@gmail.com>
2023-04-27 00:43:44 +0800

    add favicon

--- components/email-notify-new.js
+++ components/email-notify-new.js
@@ -3,16 +3,16 @@ function EmailNotifyNew() {
         <section id="email-notify" className="py-10 bg-gray-100 sm:py-16 lg:py-24">
             <div className="max-w-5xl px-4 mx-auto sm:px-6 lg:px-8">
                 <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-24 gap-y-10">
-                    <div>
-                        <h3 className="text-2xl font-semibold text-black">Why join Challengify?</h3>
-                        <p className="mt-6 text-xl leading-relaxed text-black">Challengify offers daily AI-generated challenges that can help you stay motivated and focused on your personal growth. By joining our supportive community, you'll gain access to interesting challenges, connect with like-minded individuals, and work together to achieve your goals.</p>
+                    <div className="flex flex-col justify-center">
+                        <h3 className="text-2xl font-semibold text-black">Join Challengify Today</h3>
+                        <p className="mt-6 text-xl leading-relaxed text-black">Discover AI-generated challenges that motivate personal growth. Connect with a supportive community and work together to achieve your goals.</p>
                     </div>
 
                     <div>
-                        <div className="overflow-hidden bg-white">
+                        <div className="overflow-hidden bg-white shadow-lg rounded-lg">
                             <div className="p-8 lg:px-12 lg:py-10">
-                                <h3 className="text-2xl font-semibold text-black">Join our growing community of challenge enthusiasts</h3>
-                                <p className="mt-4 text-base text-gray-600">Get daily challenge ideas directly to your inbox. Start your Challengify journey today!</p>
+                                <h3 className="text-2xl font-semibold text-black">Start your Challengify journey</h3>
+                                <p className="mt-4 text-base text-gray-600">Get daily challenge ideas delivered to your inbox.</p>
 
                                 <form className="mt-8">
                                     <input type="email" placeholder="Your email" className="w-full px-4 py-2 text-base text-gray-700 border-2 border-gray-300 rounded-md focus:outline-none focus:border-blue-600" />
--- components/guide.js
+++ components/guide.js
@@ -1,41 +1,41 @@
 function Guide() {
     return (
         <div>
-            <section id="how-it-works" class="py-10 bg-white sm:py-16 lg:py-24">
-                <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div class="max-w-2xl mx-auto text-center">
-                        <h2 class="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">How does Challngify work?</h2>
-                        <p class="max-w-lg mx-auto mt-4 text-base leading-relaxed text-gray-600">Discover a new challenge every day, commit to it, and share your progress with the Challengify community.</p>
+            <section id="how-it-works" className="py-10 bg-white sm:py-16 lg:py-24">
+                <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="max-w-2xl mx-auto text-center">
+                        <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">How does Challengify work?</h2>
+                        <p className="max-w-lg mx-auto mt-4 text-base leading-relaxed text-gray-600">Discover daily challenges, commit, and share your progress with our community.</p>
                     </div>
 
-                    <div class="relative mt-12 lg:mt-20">
-                        <div class="absolute inset-x-0 hidden xl:px-44 top-2 md:block md:px-20 lg:px-28">
-                            <img class="w-full" src="https://cdn.rareblocks.xyz/collection/celebration/images/steps/2/curved-dotted-line.svg" alt="" />
+                    <div className="relative mt-12 lg:mt-20">
+                        <div className="absolute inset-x-0 hidden xl:px-44 top-2 md:block md:px-20 lg:px-28">
+                            <img className="w-full" src="https://cdn.rareblocks.xyz/collection/celebration/images/steps/2/curved-dotted-line.svg" alt="" />
                         </div>
 
-                        <div class="relative grid grid-cols-1 text-center gap-y-12 md:grid-cols-3 gap-x-12">
+                        <div className="relative grid grid-cols-1 text-center gap-y-12 md:grid-cols-3 gap-x-12">
                             <div>
-                                <div class="flex items-center justify-center w-16 h-16 mx-auto bg-white border-2 border-gray-200 rounded-full shadow">
-                                    <span class="text-xl font-semibold text-gray-700"> 1 </span>
+                                <div className="flex items-center justify-center w-16 h-16 mx-auto bg-white border-2 border-gray-200 rounded-full shadow">
+                                    <span className="text-xl font-semibold text-gray-700"> 1 </span>
                                 </div>
-                                <h3 class="mt-6 text-xl font-semibold leading-tight text-black md:mt-10">Sign Up for Free</h3>
-                                <p class="mt-4 text-base text-gray-600">Create your Challengify account to get started with daily challenges, track your progress, and join our thriving community.</p>
+                                <h3 className="mt-6 text-xl font-semibold leading-tight text-black md:mt-10">Explore Challenges</h3>
+                                <p className="mt-4 text-base text-gray-600">Browse daily challenges and commit to one that inspires you.</p>
                             </div>
 
                             <div>
-                                <div class="flex items-center justify-center w-16 h-16 mx-auto bg-white border-2 border-gray-200 rounded-full shadow">
-                                    <span class="text-xl font-semibold text-gray-700"> 2 </span>
+                                <div className="flex items-center justify-center w-16 h-16 mx-auto bg-white border-2 border-gray-200 rounded-full shadow">
+                                    <span className="text-xl font-semibold text-gray-700"> 2 </span>
                                 </div>
-                                <h3 class="mt-6 text-xl font-semibold leading-tight text-black md:mt-10">Discover & Commit</h3>
-                                <p class="mt-4 text-base text-gray-600">Explore new challenge ideas every day, powered by GPT-4. Choose a challenge that resonates with you and commit to completing it.</p>
+                                <h3 className="mt-6 text-xl font-semibold leading-tight text-black md:mt-10">Commit & Track</h3>
+                                <p className="mt-4 text-base text-gray-600">Stay accountable and motivated as you work towards your goals.</p>
                             </div>
 
                             <div>
-                                <div class="flex items-center justify-center w-16 h-16 mx-auto bg-white border-2 border-gray-200 rounded-full shadow">
-                                    <span class="text-xl font-semibold text-gray-700"> 3 </span>
+                                <div className="flex items-center justify-center w-16 h-16 mx-auto bg-white border-2 border-gray-200 rounded-full shadow">
+                                    <span className="text-xl font-semibold text-gray-700"> 3 </span>
                                 </div>
-                                <h3 class="mt-6 text-xl font-semibold leading-tight text-black md:mt-10">Share Your Progress</h3>
-                                <p class="mt-4 text-base text-gray-600">As you work on your challenges, share your progress with the Challengify community to inspire and support others on their journey.</p>
+                                <h3 className="mt-6 text-xl font-semibold leading-tight text-black md:mt-10">Share Progress</h3>
+                                <p className="mt-4 text-base text-gray-600">Inspire and support others in the Challengify community.</p>
                             </div>
                         </div>
                     </div>
